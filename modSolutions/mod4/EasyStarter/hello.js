d9c49005024ca5d8831396812a1754a9e89ba750
Will Ospino <willospinoenglish@gmail.com>
2020-11-06 13:06:54 +0100

    attempt to transform bye.js to isolated format to not step on other scripts

--- modSolutions/mod4/EasyStarter/hello.js
+++ modSolutions/mod4/EasyStarter/hello.js
@@ -1,4 +1,7 @@
-var name = [
+var greeter = {}; // STEP2 (STEP1 is Create var greeter = {};)
+
+/*STEP 3 var - replaced w/ greeter*/ 
+greeter.name = [
     "Will",
     "Pablo",
     "Jose",
@@ -6,6 +9,7 @@ var name = [
     "Billy",
     "Rosi"
 ];
-function sayHello () {
-    console.log("Hello " + name);
+/*STEP4 - move function and make sayHello a property of greeter*/
+greeter.sayHello function () {
+    console.log("Hello " + greeter.name); /*STEP5 add greeter here too before name*/
 }
